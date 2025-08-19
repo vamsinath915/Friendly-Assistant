5cec147c5e9fdfb6e72c8d332870602bfee15435
vamsinath <vamsinath915@gmail.com>
2025-08-19 16:18:48 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -2,7 +2,8 @@ from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
     model='gemini-2.5-flash',
-    name='root_agent',
+    name='FriendlyAssistant',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
 )
+
